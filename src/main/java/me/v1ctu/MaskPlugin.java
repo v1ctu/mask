44d8128cb302d6899607d0579f3b0949abb33563
Victor Souza <victorhugochimenton@icloud.com>
2022-08-08 17:24:16 -0300

    feat: implemented vault and nbt api

--- src/main/java/me/v1ctu/MaskPlugin.java
+++ src/main/java/me/v1ctu/MaskPlugin.java
@@ -2,13 +2,20 @@ package me.v1ctu;
 
 import lombok.Getter;
 import me.saiintbrisson.bukkit.command.BukkitFrame;
-import me.v1ctu.admin.command.MaskCommand;
-import me.v1ctu.admin.command.subcommand.MaskGiveCommand;
+import me.saiintbrisson.minecraft.ViewFrame;
+import me.v1ctu.command.admin.MaskAdminCommand;
+import me.v1ctu.command.admin.subcommand.MaskGiveCommand;
+import me.v1ctu.command.regular.subcommand.MaskShopSubCommand;
+import me.v1ctu.listener.InventoryClickListener;
 import me.v1ctu.mask.Mask;
 import me.v1ctu.mask.MaskCache;
 import me.v1ctu.mask.MaskManager;
 import me.v1ctu.util.PotionEffectAdapter;
+import me.v1ctu.view.MaskShopView;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffectType;
 
@@ -19,14 +26,36 @@ public class MaskPlugin extends JavaPlugin {
     private MaskCache maskCache;
     private MaskManager maskManager;
 
+    @Getter
+    private ViewFrame viewFrame;
+
+    @Getter
+    private Economy economy;
+
+    @SuppressWarnings("UnstableApiUsage")
     @Override
     public void onEnable() {
         maskCache = new MaskCache();
         maskManager = new MaskManager(maskCache, this);
 
+        if (!setupEconomy() ) {
+            getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
         saveDefaultConfig();
         loadCache();
+
+        viewFrame = new ViewFrame(this);
+        viewFrame.register(
+            new MaskShopView(maskManager, this)
+        );
+
         registerCommands();
+
+        final PluginManager pluginManager = getServer().getPluginManager();
+        pluginManager.registerEvents(new InventoryClickListener(maskManager), this);
     }
 
     @Override
@@ -34,11 +63,24 @@ public class MaskPlugin extends JavaPlugin {
 
     }
 
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        economy = rsp.getProvider();
+        return economy != null;
+    }
+
     private void registerCommands() {
         BukkitFrame bukkitFrame = new BukkitFrame(this);
         bukkitFrame.registerCommands(
-            new MaskCommand(),
-            new MaskGiveCommand(maskManager));
+            new MaskAdminCommand(),
+            new MaskGiveCommand(maskManager),
+            new MaskShopSubCommand(this));
     }
 
     private void loadCache() {
@@ -66,6 +108,5 @@ public class MaskPlugin extends JavaPlugin {
             );
             maskCache.put(mask.getName(), mask);
         }
-        System.out.println("maskCache.get() = " + maskCache.get());
     }
 }
